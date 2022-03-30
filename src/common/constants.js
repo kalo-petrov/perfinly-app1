b2b0dcc6c8dce5df7cd198a7a9a9a6d00618ced0
Kalo Petrov <kalo.petrv@gmail.com>
2022-03-30 14:28:10 -0500

    api ENV set

--- src/common/constants.js
+++ src/common/constants.js
@@ -1,1 +1,4 @@
-export const BASE_URL = 'http://localhost:4000/api';
+export const BASE_URL =
+  process.env.NODE_ENV === 'development'
+    ? 'http://localhost:8080/api'
+    : 'http://app-5a018d47-0af1-41dd-831e-ba2c5a2b3ced.cleverapps.io/api';
