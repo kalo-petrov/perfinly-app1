6b7c7b1a379a3c6ac69dff3ef71e97dc4d5298b9
Kalo Petrov <kalo.petrv@gmail.com>
2022-04-07 14:27:14 -0500

    admin dashboard improvements

--- src/components/Admin/AdminDashboard/AdminDashboard.js
+++ src/components/Admin/AdminDashboard/AdminDashboard.js
@@ -3,10 +3,12 @@ import './AdminDashboard.css';
 import httpProvider from './../../../providers/httpProvider';
 import { BASE_URL } from '../../../common/constants';
 import moment, { now } from 'moment';
+import useSortableData from './../../../hooks/useSortableData';
 
 const AdminDashboard = () => {
   const [activeUsers1Hour, setActiveUsers1Hour] = React.useState([]);
   const [activeUsers1Day, setActiveUsers1Day] = React.useState([]);
+  const [activeUsers1Month, setActiveUsers1Month] = React.useState([]);
   const [feedback, setFeedback] = React.useState([]);
 
   React.useEffect(() => {
@@ -15,7 +17,6 @@ const AdminDashboard = () => {
         if (data.error) {
           return;
         }
-        console.log(data);
         setActiveUsers1Hour(
           data.users.filter(
             (u) => u.last_login && moment(u.last_login).add(1, 'hours') >= moment(new Date())
@@ -26,19 +27,25 @@ const AdminDashboard = () => {
             (u) => u.last_login && moment(u.last_login).add(1, 'day') >= moment(new Date())
           )
         );
+        setActiveUsers1Month(
+          data.users.filter(
+            (u) => u.last_login && moment(u.last_login).add(1, 'month') >= moment(new Date())
+          )
+        );
       });
 
       await httpProvider.get(`${BASE_URL}/feedback/all`).then((data) => {
         if (data.error) {
           return;
         }
-        console.log(data);
         setFeedback(data.slice(0, 5));
       });
     };
 
     fetchData();
   }, []);
+
+  const { items, requestSort } = useSortableData(feedback);
 
   return (
     <div>
@@ -61,24 +68,47 @@ const AdminDashboard = () => {
             <h1> {activeUsers1Day.length}</h1>
           </div>
         </div>
-
-      </div>
-      <div>
+        <div className='admin-dashboard-item'>
           <div>
-            <h5>Latest Feedback</h5>
-          </div>
+            <h5>Active Users in Last 1 Month</h5>
+          </div>{' '}
           <div>
-            {' '}
-            {feedback?.map((f) => {
-              return (
-                <div className='dashboard-feedback-item' key={f._id}>
-                  {f.title}
-                  <div>{f.feedback}</div>
-                </div>
-              );
-            })}
+            <h1> {activeUsers1Month.length}</h1>
           </div>
         </div>
+      </div>
+      <div>
+        <div>
+          <h5>Latest Rating</h5>
+        </div>
+        <div className='latest-rating-container'>
+          <table>
+            <tr>
+              <th onClick={() => requestSort('username')}>User</th>
+              <th onClick={() => requestSort('rating')}>Rating</th>
+              <th onClick={() => requestSort('date_created')}>Date</th>
+            </tr>
+            <tbody>
+              {items?.map((f) => {
+                return (
+                  <tr className='dashboard-feedback-item' key={f._id}>
+                    <td>{f.username}</td>
+                    <td>{f.rating}</td>
+                    <td>{f.date_created.slice(0,10)}</td>
+                  </tr>
+                );
+              })}
+              <tr>
+                <th>Average</th>
+                <th>{feedback.reduce((acc, f) => {
+                  acc += f.rating
+                  return acc;
+                }, 0)/feedback.length}</th>
+              </tr>
+            </tbody>
+          </table>{' '}
+        </div>
+      </div>
     </div>
   );
 };
--- src/components/Admin/AllFeedback/AllFeedback.js
+++ src/components/Admin/AllFeedback/AllFeedback.js
@@ -22,48 +22,101 @@ const AllFeedback = () => {
     fetchData();
   }, []);
 
-  const { items, requestSort, sortConfig } = useSortableData(feedback);
+  const { items, requestSort } = useSortableData(feedback);
 
   return (
     <div>
       <h3>All Feedback</h3>
 
       <div className='feedback-container'>
-        <table>
+        <table className='feedback-table'>
           <thead>
             <tr>
               <th onClick={() => requestSort('username')}>Username</th>
               <th onClick={() => requestSort('title')}>Title</th>
               <th onClick={() => requestSort('rating')}>Rating</th>
+              <th onClick={() => requestSort('usage')}>Will Use</th>
+              <th onClick={() => requestSort('financialHabits')}>Financial Habits</th>
+              <th onClick={() => requestSort('intuitiveUse')}>Inituitive Use</th>
+              <th onClick={() => requestSort('careAboutDesign')}>Cares About Design</th>
+              <th onClick={() => requestSort('design')}>Design</th>
+              <th onClick={() => requestSort('recommend')}>Recommend To Friend</th>
               <th onClick={() => requestSort('feedback')}>Feedback1</th>
               <th onClick={() => requestSort('feedback')}>Feedback2</th>
               <th onClick={() => requestSort('feedback')}>Feedback3</th>
               <th onClick={() => requestSort('feedback')}>Feedback4</th>
               <th onClick={() => requestSort('date_created')}>Date Created </th>
-              <th onClick={() => requestSort('date_edited')}>Date Edited </th>
             </tr>
           </thead>
           <tbody>
-        {items?.map((f) => {
-          return (
-            <tr key={f._id}>
-              <td>{f.username}</td>
-              <td>{f.title}</td>
-              <td>{f.rating}</td>
-              <td>{f.feedback[0]}</td>
-              <td>{f.feedback[1]}</td>
-              <td>{f.feedback[2]}</td>
-              <td>{f.feedback[3]}</td>
-              <td>{moment(f.date_created).format('DD-MMM-YYYY')}</td>
-              <td>{f.date_edited && moment(f.date_edited).format('DD-MMM-YYYY')}</td>
+            {items?.map((f) => {
+              return (
+                <tr key={f._id}>
+                  <td>{f.username}</td>
+                  <td>{f.title}</td>
+                  <td>{f.rating}</td>
+                  <td>{f.usage}</td>
+                  <td>{f.financialHabits}</td>
+                  <td>{f.intuitiveUse}</td>
+                  <td>{f.careAboutDesign}</td>
+                  <td>{f.design}</td>
+                  <td>{f.recommend}</td>
+                  <td>{f.feedback[0]}</td>
+                  <td>{f.feedback[1]}</td>
+                  <td>{f.feedback[2]}</td>
+                  <td>{f.feedback[3]}</td>
+                  <td>{moment(f.date_created).format('DD-MMM-YYYY')}</td>
+                </tr>
+              );
+            })}
+            <tr>
+              <th>Averages</th>
+              <th>-</th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.rating;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.usage || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.financialHabits || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.intuitiveUse || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.careAboutDesign || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.design || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
+              <th>
+                {feedback.reduce((acc, f) => {
+                  acc += f.recommend || 0;
+                  return acc;
+                }, 0) / feedback.length}
+              </th>
             </tr>
-          );
-        })}
-          
-        </tbody>
+          </tbody>
         </table>
-        
-
       </div>
     </div>
   );
